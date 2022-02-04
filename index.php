db67f5175651fa21d83068538809e515d5b10010
eugenesm <eugene@wearesuperb.com>
2022-02-05 01:30:11 +0200

    fixes

--- index.php
+++ index.php
@@ -7,9 +7,52 @@ if(isset($_SESSION["login"]) && isset($_SESSION["password"])){
 }
 ?>
 <?php  include_once 'templates/head.php'; ?>
-<form action="action/login.php" method="post">
-    <input type="text" name="login">
-    <input type="text" name="password">
-
-    <button type="submit">Login</button>
-</form>
+<div id="login">
+    <div class="container">
+        <div id="login-row" class="row justify-content-center align-items-center">
+            <div id="login-column" class="col-md-6">
+                <div id="login-box" class="col-md-12">
+                    <form id="login-form" class="form" action="/action/login.php" method="post">
+                        <h3 class="text-center text-info">Login</h3>
+                        <div class="form-group">
+                            <label for="login" class="text-info">Login:</label><br>
+                            <input type="text" name="login" id="username" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="password" class="text-info">Password:</label><br>
+                            <input type="text" name="password" id="password" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <input type="submit" name="submit" class="btn btn-info btn-md" value="submit">
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<style>
+    body {
+        margin: 0;
+        padding: 0;
+        background-color: #17a2b8;
+        height: 100vh;
+    }
+    #login .container #login-row #login-column #login-box {
+        margin-top: 120px;
+        max-width: 600px;
+        height: 320px;
+        border: 1px solid #9C9C9C;
+        background-color: #EAEAEA;
+    }
+    #login .container #login-row #login-column #login-box #login-form {
+        padding: 20px;
+    }
+    #login .container #login-row #login-column #login-box #login-form #register-link {
+        margin-top: -85px;
+    }
+    .form-group
+    {
+        margin-bottom: 20px;
+    }
+</style>
